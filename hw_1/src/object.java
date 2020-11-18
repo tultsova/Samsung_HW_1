49cdf540460044427470cbcf9d44738ad8b5bde7
ud <tultsova.a.d@edu.mirea.ru>
2020-11-18 11:17:38 +0300

    hw_1

---
+++ hw_1/src/object.java
@@ -0,0 +1,12 @@
+// Даны 2 целых числа: A и B. Требуется вычислить их сумму.
+import java.util.Scanner;
+public class object {
+    public static void  main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        int b;
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a+b);
+    }
+}
